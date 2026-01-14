2b2db09e7eb0e3fe8e6d4d755d2fa1315e1461e6
Edazawa Takuma <t.edazawa@itreat.co.jp>
2026-01-14 16:41:54 +0900

    Revert "テスト１です"
    This reverts commit 99ecac6e8587f17dd570896b6d1bb6f4d71f1047.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -76,7 +76,12 @@ class PostController extends Controller
         }
         
 
-        
+        $username = $validated['username'] ?? '';
+        $title = $validated['title'] ?? '';
+        $user_id = $validated['user_id'] ?? '';
+        $freeword = $validated['freeword'] ?? '';
+        $time_st = $validated['time_st'] ?? '';
+        $time_fn = $validated['time_fn'] ?? '';
 
         
 
