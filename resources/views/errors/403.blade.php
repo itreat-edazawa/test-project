21222852d8a1e9f039ad0b9baf21b704e57aea07
Edazawa Takuma <t.edazawa@itreat.co.jp>
2026-01-30 14:23:02 +0900

    403エラー画面の作成

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,12 @@
 <x-app-layout>
-    <div>
-        このサイトのアクセス権限はありません
+    <div class="w-full bg-slate-50 mx-10">
+        <div class="my-2 text-3xl text-center">
+            <p>{{"403エラー:このサイトに入る権限はありません"}}</p>
+        </div>
+        <div class="text-center">
+            <button class="m-10 py-4 px-6 bg-green-400 hover:bg-green-300 text-gray-600 hover:text-gray-500 rounded text-xl" type="button" onclick="location.href='{{route('post.index')}}'" >
+                {{"投稿一覧に戻る"}}
+            </button>
+        </div>
     </div>
 </x-app-layout>
