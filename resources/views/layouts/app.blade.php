4061dd2269fed997c119ec23c549bea1b9ec0a5e
Edazawa Takuma <t.edazawa@itreat.co.jp>
2026-02-04 14:14:52 +0900

    <link>に再び戻した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="/build/app.css">
+        <script src="/build/app.js"></script>
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
