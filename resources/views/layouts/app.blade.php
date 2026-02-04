c7a58eb6fcee114e8f7a524377c99fe3fa9e405b
Edazawa Takuma <t.edazawa@itreat.co.jp>
2026-02-04 14:25:11 +0900

    vercel.jsonの方を忘れてた、こっちのパスを少し変えた

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="/build/app.css">
-        <script src="/build/app.js"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="/build/index.css">
+    @vite(['resources/css/index.css'])
 </head>
 <x-app-layout>
     <x-slot name="header">
